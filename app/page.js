13d2801fbdd2fa3d52d2e5b4ccf3ca603949fc61
Sebastian Medina <snmedina91@gmail.com>
2023-10-18 17:53:11 -0300

    2d commit

--- app/page.js
+++ app/page.js
@@ -5,30 +5,31 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col ">
-  <div className="w-full text-center">
-    <h1>Escena 1</h1>
-  </div>
-  <ButtonSound soundName="Telefonos" />
-  <ButtonSound soundName="Disparos" />
-  <ButtonSound soundName="Besame" />
-  <ButtonSound soundName="Puerta" />
+        <div className="w-full text-center">
+          <h1>Escena 1</h1>
+        </div>
+        <ButtonSound soundName="Telefonos" />
+        <ButtonSound soundName="Disparos" />
+        <ButtonSound soundName="Besame" />
+        <ButtonSound soundName="Puerta" />
 
-  <div className="w-full text-center">
-    <h1>Escena 2</h1>
-  </div>
-  <ButtonSound soundName="Telefonos" />
-  <ButtonSound soundName="Disparos" />
-  <ButtonSound soundName="Besame" />
-  <ButtonSound soundName="Puerta" />
+        <div className="w-full text-center">
+          <h1>Escena 7</h1>
+        </div>
+        <ButtonSound soundName="Telefonos2" />
+        <ButtonSound soundName="Disparos2" />
+        <ButtonSound soundName="Besame2" />
+        <ButtonSound soundName="Puerta" />
 
-  <div className="w-full text-center">
-    <h1>Escena 3</h1>
-  </div>
-  <ButtonSound soundName="Telefonos" />
-  <ButtonSound soundName="Disparos" />
-  <ButtonSound soundName="Besame" />
-  <ButtonSound soundName="Puerta" />
-</div>
+        <div className="w-full text-center">
+          <h1>Escena 8</h1>
+        </div>
+
+        <ButtonSound soundName="Besame3" />
+        <ButtonSound soundName="Besame3Alto" />
+        <ButtonSound soundName="Besame3Bajo" />
+        <ButtonSound soundName="Bomba" />
+      </div>
     </>
   );
 }
--- components/ButtonSound.jsx
+++ components/ButtonSound.jsx
@@ -5,12 +5,20 @@ import Telefono from "../assets/Telefonos.mp3";
 import Disparos from "../assets/disparos.mp3";
 import Besame from "../assets/BesameMucho.mp3";
 import PuertayPasos from "../assets/PuertayPasos.mp3";
+import Bomba from "../assets/bomba.mp3";
 
 const sounds = {
     Telefonos: new Howl({ src: [Telefono] }),
+    Telefonos2: new Howl({ src: [Telefono] }),
     Disparos: new Howl({ src: [Disparos] }),
+    Disparos2: new Howl({ src: [Disparos] }),
     Besame: new Howl({ src: [Besame] }),
+    Besame2: new Howl({ src: [Besame] }),
+    Besame3: new Howl({ src: [Besame], volume: 1.0 }),
+    Besame3Alto: new Howl({ src: [Besame], volume: 1.5 }), // Volumen alto
+    Besame3Bajo: new Howl({ src: [Besame], volume: 0.5 }),
     Puerta: new Howl({ src: [PuertayPasos] }),
+    Bomba: new Howl({ src: [Bomba] }),
     // Agrega más sonidos aquí
   };
 
