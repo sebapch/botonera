f42888ca03ae848d7a8dd727a31c2c450487cc0a
Sebastian Medina <snmedina91@gmail.com>
2023-10-25 17:15:35 -0300

    fixes 2

--- app/page.js
+++ app/page.js
@@ -1,34 +1,58 @@
 import ButtonSound from "@/components/ButtonSound";
 import Image from "next/image";
+import Telefono from "../assets/Telefonos.mp3";
+import Disparos from "../assets/disparos.mp3";
+import Besame from "../assets/BesameMucho.mp3";
+import PuertayPasos from "../assets/PuertayPasos.mp3";
+import Bomba from "../assets/bomba.mp3";
+import Disparo from "../assets/Disparo2.mp3";
+import Silvido from "../assets/Silbido.mp3";
+import Campanita from "../assets/Campanitas.mp3";
+import DisparosFX from "../assets/DisparosFX.mp3";
+import ButtonSound2 from "@/components/ButtonSound2";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col ">
         <div className="w-full text-center">
-          <h1>Escena 1</h1>
+          <h1 className="mt-4">Escena 1</h1>
         </div>
-        <ButtonSound soundName="Telefonos" />
-        <ButtonSound soundName="Disparos" />
-        <ButtonSound soundName="Besame" />
-        <ButtonSound soundName="Puerta" />
+        <h4>Telefonos de oficina</h4>
+        <ButtonSound2 soundName={Telefono} />
+        <h4>Disparos</h4>
+        <ButtonSound soundName={Disparos} />
+        <h4>Besame Mucho</h4>
+        <ButtonSound soundName={Besame} />
+        <h4>Puerta y pasos</h4>
+        <ButtonSound soundName={PuertayPasos} />
+
+
+        <div className="w-full text-center">
+          <h1>Escena 6</h1>
+        </div>
+        <h4>Disparo accidental</h4>
+        <ButtonSound soundName={Disparo} />
 
         <div className="w-full text-center">
           <h1>Escena 7</h1>
         </div>
-        <ButtonSound soundName="Telefonos2" />
-        <ButtonSound soundName="Disparos2" />
-        <ButtonSound soundName="Besame2" />
-        <ButtonSound soundName="Puerta" />
+        <h4>Telefonos de oficina</h4>
+        <ButtonSound2 soundName={Telefono} />
+        <h4>Disparos</h4>
+        <ButtonSound soundName={Disparos} />
+        <h4>Disparos Locos</h4>
+        <ButtonSound soundName={DisparosFX} />
+        <h4>Besame Silbido</h4>
+        <ButtonSound soundName={Silvido} />
 
         <div className="w-full text-center">
           <h1>Escena 8</h1>
         </div>
-
-        <ButtonSound soundName="Besame3" />
-        <ButtonSound soundName="Besame3Alto" />
-        <ButtonSound soundName="Besame3Bajo" />
-        <ButtonSound soundName="Bomba" />
+        <h4>Besame mucho con volumen</h4>
+        <ButtonSound2 soundName={Besame} />
+        <h4>Bomba</h4>
+        <ButtonSound soundName={Bomba} />
       </div>
     </>
   );
--- components/ButtonSound.jsx
+++ components/ButtonSound.jsx
@@ -1,53 +1,22 @@
 "use client";
-import React, {useState} from "react";
-import { Howl, Howler } from "howler";
-import Telefono from "../assets/Telefonos.mp3";
-import Disparos from "../assets/disparos.mp3";
-import Besame from "../assets/BesameMucho.mp3";
-import PuertayPasos from "../assets/PuertayPasos.mp3";
-import Bomba from "../assets/bomba.mp3";
+import React, {useState, useRef} from "react";
 
-const sounds = {
-    Telefonos: new Howl({ src: [Telefono] }),
-    Telefonos2: new Howl({ src: [Telefono] }),
-    Disparos: new Howl({ src: [Disparos] }),
-    Disparos2: new Howl({ src: [Disparos] }),
-    Besame: new Howl({ src: [Besame] }),
-    Besame2: new Howl({ src: [Besame] }),
-    Besame3: new Howl({ src: [Besame], volume: 1.0 }),
-    Besame3Alto: new Howl({ src: [Besame], volume: 1.5 }), // Volumen alto
-    Besame3Bajo: new Howl({ src: [Besame], volume: 0.5 }),
-    Puerta: new Howl({ src: [PuertayPasos] }),
-    Bomba: new Howl({ src: [Bomba] }),
-    // Agrega más sonidos aquí
-  };
+
 
 
 const ButtonSound = ({ soundName }) => {
-    const sound = sounds[soundName];
-    const [isPlaying, setIsPlaying] = useState(false);
-  
-    const playPauseSound = () => {
-      if (sound) {
-        if (isPlaying) {
-          sound.pause();
-        } else {
-          sound.play();
-        }
-        setIsPlaying(!isPlaying);
-      }
-    };
+    
 
+  //this is telefono
+  console.log(soundName) 
   return (
     <>
-      <div className="w-[95%]">
-        <button
-        className="w-full h-16 bg-blue-500 text-white text-lg font-bold rounded-md p-2 m-2"
-        onClick={playPauseSound}
-        >
-          {isPlaying ? "Pausa" : soundName}
-        </button>
-      </div>
+        <div >
+      <audio controls className="w-[95%]">
+        <source src={soundName} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
     </>
   );
 };
